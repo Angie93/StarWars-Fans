e72cb0d6afabe1b49c753bdf5cec6c6f6ff8e4b8
avelilag <avelilag@everis.com>
2018-08-25 09:19:13 +0200

    Create Project

---
+++ StarWars Fans/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StarWars Fans
+//
+//  Created by Angela Velilla Aguirre on 25/8/18.
+//  Copyright © 2018 Angela Velilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
