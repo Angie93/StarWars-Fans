a686127fea16d1712289eb48b257880f7b47bdb6
avelilag <avelilag@everis.com>
2018-08-25 17:17:40 +0200

    characters View

---
+++ StarWars Fans/View/Cells/CharacterTableViewCell.swift
@@ -0,0 +1,60 @@
+//
+//  CharacterTableViewCell.swift
+//  StarWars Fans
+//
+//  Created by Angela Velilla Aguirre on 25/8/18.
+//  Copyright © 2018 Angela Velilla. All rights reserved.
+//
+
+import UIKit
+
+class CharacterTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var characterImage: UIImageView!
+    @IBOutlet weak var characterName: UILabel!
+    @IBOutlet weak var characterGender: UILabel!
+    @IBOutlet weak var characterBirthday: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func loadLocalizedResources() {
+        characterName.font = StyleController.fontArialBoldWIthSize(size:18)
+        characterName.textColor = StyleController.mainYellowColor()
+        characterGender.font = StyleController.fontArialRegularWIthSize(size:16)
+        characterGender.textColor = UIColor.white
+        characterBirthday.font = StyleController.fontArialRegularWIthSize(size:16)
+        characterBirthday.textColor = UIColor.white
+    }
+    
+    func updateCellWithCharacter(_ character: Character) {
+        resetCell()
+        let url = URL(string: character.image)
+        let data = try? Data(contentsOf: url!)
+        
+        if let imageData = data {
+            let image = UIImage(data: imageData)
+            self.characterImage.image = image
+        }
+        
+        self.characterName.text = character.name
+        self.characterGender.text = character.gender
+        self.characterBirthday.text = character.birthYear
+    }
+    
+    func resetCell() {
+        self.characterImage.image = nil
+        self.characterName.text = ""
+        self.characterGender.text = ""
+        self.characterBirthday.text = ""
+    }
+    
+}
--- StarWars Fans/View/ViewController/CharactersTableViewController.swift
+++ StarWars Fans/View/ViewController/CharactersTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FilmsTableViewController.swift
+//  CharactersTableViewController.swift
 //  StarWars Fans
 //
 //  Created by Angela Velilla Aguirre on 25/8/18.
@@ -8,31 +8,32 @@
 
 import UIKit
 
-class FilmsTableViewController: UITableViewController {
+class CharactersTableViewController: UITableViewController {
 
-    var filmsArray:[Film] = []
+    var charactersArray:[Character] = []
     var hasError = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Films"
-        self.tableView.register(UINib(nibName: "FilmTableViewCell", bundle: nil), forCellReuseIdentifier: "filmCell")
+        
+        self.title = "Characters"
+        self.navigationController?.title = "Characters"
+        self.tableView.register(UINib(nibName: "CharacterTableViewCell", bundle: nil), forCellReuseIdentifier: "characterCell")
         self.tableView.register(UINib(nibName: "ErrorTableViewCell", bundle: nil), forCellReuseIdentifier: "errorCell")
         self.tableView.delegate = self
         self.tableView.dataSource = self
-        startFetchingFilms()
+        fetchCharacters()
     }
-    
-    fileprivate func startFetchingFilms() {
+
+    fileprivate func fetchCharacters() {
         
-        FilmsController().fetchStarWarsFilms(onSuccess: { (filmsArray) in
+        CharactersController().fetchStarWarsAllCharacters(onSuccess: { (charactersArray) in
             self.hasError = false
-            self.filmsArray = filmsArray
-            self.tableView.rowHeight = 220
+            self.charactersArray = charactersArray
+            self.tableView.rowHeight = 100
             DispatchQueue.main.async {
                 self.tableView.reloadData()
             }
-            
         }) { (error) in
             self.hasError = true
             self.tableView.rowHeight = self.tableView.frame.height - 100
@@ -40,18 +41,17 @@ class FilmsTableViewController: UITableViewController {
                 self.tableView.reloadData()
             }
         }
-        
     }
-    
     // MARK: - Table view data source
-    
+
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return hasError ? 1 : filmsArray.count
+        return hasError ? 1 : charactersArray.count
     }
+
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if hasError {
@@ -59,27 +59,21 @@ class FilmsTableViewController: UITableViewController {
             let cell: ErrorTableViewCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! ErrorTableViewCell
             return cell
         } else {
-            let cellIdentifier = "filmCell"
-            let cell: FilmTableViewCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! FilmTableViewCell
+            let cellIdentifier = "characterCell"
+            let cell: CharacterTableViewCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! CharacterTableViewCell
             
-            cell.updateCellWithFilm(filmsArray[indexPath.row])
+            cell.updateCellWithCharacter(charactersArray[indexPath.row])
             
             return cell
         }
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if hasError {
-            startFetchingFilms()
-            
-        } else {
-            
-        }
-        tableView.deselectRow(at: indexPath, animated: true)
-//        productSelected = productsArray[indexPath.row]
-//        performSegue(withIdentifier: "segueToDetail", sender: self)
+    // MARK: - Navigation
 
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
-    
-}
 
+}
