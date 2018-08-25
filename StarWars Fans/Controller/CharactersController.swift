8cee82d74e6db70a677a12381381b3609066639c
avelilag <avelilag@everis.com>
2018-08-25 10:13:08 +0200

    Characters Controller

--- StarWars Fans/Controller/CharactersController.swift
+++ StarWars Fans/Controller/CharactersController.swift
@@ -11,13 +11,16 @@ import Alamofire
 
 class CharactersController {
     
-    func fetchStarWarsFilms( onSuccess: (([Character]) -> Void)?,
-                             onError: ((_ error: Error?) -> Void)?) {
+    func fetchStarWarsAllCharacters( onSuccess: @escaping (([Character]) -> Void),
+                             onError: @escaping ((_ error: NSError?) -> Void)) {
         
         let url = URL(string: "https://swapi.co/api/people/")
         var charactersResult: [Character] = []
         
-        Alamofire.request(url!, method: .get, parameters: ["include_docs": "true"], encoding: JSONEncoding.default)
+        Alamofire.request(url!,
+                          method: .get,
+                          parameters: ["include_docs": "true"],
+                          encoding: JSONEncoding.default)
             .responseJSON { response in
                 print(response)
                 //to get status code
@@ -26,7 +29,7 @@ class CharactersController {
                     case 201:
                         print("example success")
                     default:
-                        print("error with response status: \(status)")
+                        onError(self.createCharacterError())
                     }
                 }
                 //get JSON return value
@@ -39,22 +42,68 @@ class CharactersController {
                         for currentChar in charactersArray {
                             if let characterDict = currentChar as? [String:Any] {
                                 let character = self.mappCharacters(characterDict: characterDict)
-                                //Hacer la comprobacion de si character no es nil
+                                
                                 if let characterOK = character {
                                     charactersResult.append(characterOK)
-                                } else {
-                                    let error: Error = Error()
-                                    onError(
                                 }
                             }
-                            
                         }
                     }
                     
+                    if charactersResult.count > 0 {
+                        onSuccess(charactersResult)
+                    } else {
+                        onError(self.createCharacterError())
+                    }
+                } else {
+                    onError(self.createCharacterError())
                 }
                 
         }
         
+    }
+    
+    func fetchStarWarsSpecificCharacter( urlString: String,
+                                         onSuccess: @escaping ((Character) -> Void),
+                                         onError: @escaping ((_ error: NSError?) -> Void)) {
+        
+        let url = URL(string: urlString)
+        
+        Alamofire.request(url!,
+                          method: .get,
+                          parameters: ["include_docs": "true"],
+                          encoding: JSONEncoding.default)
+            .responseJSON { response in
+                print(response)
+                //to get status code
+                if let status = response.response?.statusCode {
+                    switch(status){
+                    case 201:
+                        print("example success")
+                    default:
+                        onError(self.createCharacterError())
+                    }
+                }
+                //get JSON return value
+                if let result = response.result.value {
+                    let JSON = result as! NSDictionary
+                    print(JSON)
+                    
+                    if let characterDict = JSON as? [String:Any] {
+                        let character = self.mappCharacters(characterDict: characterDict)
+                        
+                        if let characterOK = character {
+                            onSuccess(characterOK)
+                        } else {
+                            onError(self.createCharacterError())
+                        }
+                    } else {
+                        onError(self.createCharacterError())
+                    }
+                } else {
+                    onError(self.createCharacterError())
+                }
+        }
     }
     
     fileprivate func mappCharacters(characterDict: [String: Any]) -> Character? {
@@ -76,5 +125,9 @@ class CharactersController {
                              gender: gender)
         }
         return nil
+    }
+    
+    fileprivate func createCharacterError() -> NSError {
+        return NSError(domain:"Error creating characters", code:0, userInfo:nil)
     }
 }
